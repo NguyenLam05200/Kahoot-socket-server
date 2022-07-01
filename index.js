7728a3359f32d241291885fe0028e25938b28171
Nguyễn Văn Lâm <60528316+NguyenLam05200@users.noreply.github.com>
2022-07-01 18:24:55 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,113 @@
+const express = require("express")
+var app = express();
+var server = app.listen(8080, () => {
+    console.log('listening on 8080');
+});
+var io = require('socket.io')(server, {
+    cors: {
+        origin: '*',
+    }
+});
+let listPinCurrents = [];
+let listRoomKahuts = new Map();
+// let listHosts = new Map();
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    console.log('id of client: ', socket.id);
+    io.to(socket.id).emit('chat message', 'hi');
+
+    socket.on('HAND_SHAKE', () => {
+        io.to(socket.id).emit('HAND_SHAKE', 'hehe');
+    });
+
+    // host:
+    socket.on('CREATE_PIN', (listQuestions) => {
+        let newPin = 0;
+        while (newPin === 0 || listPinCurrents.includes(newPin)) {
+            newPin = getRndInteger(100000, 999999);
+        }
+        listPinCurrents.push(newPin);
+        listRoomKahuts.set(newPin, {
+            hostId: socket.id,
+            acceptJoin: true,
+            listQuestions: listQuestions,
+            listPlayer: new Map(),
+        })
+        // listHosts.set(socket.id, newPin)
+
+        socket.join(newPin);
+        socket.host = newPin;
+
+        io.to(socket.id).emit('CREATE_PIN', newPin);
+    });
+
+    socket.on('BLOCK_JOIN', () => {
+        listRoomKahuts.get(socket.host).acceptJoin = !listRoomKahuts.get(socket.host).acceptJoin;
+    });
+
+    // player:
+    socket.on('ENTER_PIN', (pinInput) => {
+        pinInput = parseInt(pinInput)
+
+        if (listPinCurrents.includes(pinInput)) {
+            if (listRoomKahuts.get(pinInput).acceptJoin) {
+                let listQues = JSON.parse(JSON.stringify(listRoomKahuts.get(pinInput).listQuestions));
+                listQues = listQues.map((eachQuestion) => {
+                    return {
+                        type: eachQuestion.type,
+                        time: eachQuestion.time,
+                        ansAmount: eachQuestion.ans.length,
+                    }
+                });
+
+                socket.join(pinInput);
+                socket.pin = pinInput;
+
+                io.to(socket.id).emit('ENTER_PIN', { isRightPin: true, listQuestions: listQues });
+            } else {
+                io.to(socket.id).emit('ENTER_PIN', { isRightPin: false, errMsg: "Host didn't accept to join" });
+            }
+        } else {
+            io.to(socket.id).emit('ENTER_PIN', { isRightPin: false, errMsg: 'Your pin is incorrect' });
+        }
+
+    });
+
+    socket.on('ENTER_NAME', (nameInput) => {
+        listRoomKahuts.get(socket.pin).listPlayer.set(socket.id, { name: nameInput, score: 0 })
+        socket.name = nameInput;
+        io.to(listRoomKahuts.get(socket.pin).hostId).emit('PLAYER_JOIN', { id: socket.id, name: nameInput })
+    });
+
+    socket.on('disconnect', () => {
+
+        const hostPin = socket.host;
+
+        if (hostPin) {
+            listRoomKahuts.delete(hostPin);
+            var myIndex = listPinCurrents.indexOf(hostPin);
+            if (myIndex !== -1) {
+                listPinCurrents.splice(myIndex, 1);
+            }
+        }
+        const playerPin = socket.pin;
+        if (playerPin) {
+            if (listRoomKahuts.get(playerPin)) {
+                listRoomKahuts.get(playerPin).listPlayer.delete(socket.id)
+                io.to(listRoomKahuts.get(socket.pin).hostId).emit('PLAYER_LEAVE', socket.id)
+            }
+            socket.leave(playerPin)
+        }
+
+        console.log('user disconnected');
+    });
+});
